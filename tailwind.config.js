9930ed57ef169acb67051bcf2adf6f10a69216b1
Eden <64953425+Solaireis@users.noreply.github.com>
2024-10-04 03:06:48 +0800

    edit file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "assets/**/*.{html,js}",
-    "index.html",
+    "./docs/assets/**/*.{html,js}",
+    "./docs/index.html",
   ],
   theme: {
     extend: {},
